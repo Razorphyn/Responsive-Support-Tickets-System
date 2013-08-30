2130d7c1c5f5c0f9098f6d250c8ab615899b67d8
Razorphyn <est.grandi@gmail.com>
2013-08-30 13:12:57 +0200

    Bug Fix
    Fixed Queries

--- php/sendmail.php
+++ php/sendmail.php
@@ -40,10 +40,45 @@ if(isset($argv[0]) && isset($argv[1]) && isset($argv[2])){
 		case 'NewRep':
 			if(is_file('../php/config/mail/newreply.txt')){
 				$file=file('../php/config/mail/newreply.txt',FILE_IGNORE_NEW_LINES);
-				if($argv[2]==1)//utente
-					$query="SELECT a.ref_id,CASE a.priority WHEN '0' THEN 'Low' WHEN '1' THEN 'Medium' WHEN '2' THEN 'High' WHEN '3' THEN 'Urgent' WHEN '4' THEN 'Critical' ELSE 'Error' END, CASE a.ticket_status WHEN '0' THEN 'Closed' WHEN '1' THEN 'Open' WHEN '2' THEN 'To Assign' ELSE 'Error' END ,a.title, b.name ,c.department_name, tu.mail,tu.name FROM ".$SupportTicketsTable." a, ".$SupportUserTable." b, ".$SupportDepaTable." c,".$SupportTicketsTable." lt,".$SupportUserTable." tu WHERE a.enc_id=? AND b.id=a.user_id AND c.id=a.department_id  AND tu.id=lt.operator_id LIMIT 1;";
-				else//operatore
-					$query="SELECT a.ref_id,CASE a.priority WHEN '0' THEN 'Low' WHEN '1' THEN 'Medium' WHEN '2' THEN 'High' WHEN '3' THEN 'Urgent' WHEN '4' THEN 'Critical' ELSE 'Error' END, CASE a.ticket_status WHEN '0' THEN 'Closed' WHEN '1' THEN 'Open' WHEN '2' THEN 'To Assign' ELSE 'Error' END ,a.title, b.name , b.mail_alert ,c.department_name, b.mail,tu.name FROM ".$SupportTicketsTable." a, ".$SupportUserTable." b, ".$SupportDepaTable." c,".$SupportTicketsTable." lt,".$SupportUserTable." tu WHERE a.enc_id=? AND b.id=a.user_id AND c.id=a.department_id  AND tu.id=lt.operator_id LIMIT 1;";
+				if($argv[2]==1)//user
+					$query="SELECT 
+									a.ref_id,
+									CASE a.priority WHEN '0' THEN 'Low' WHEN '1' THEN 'Medium' WHEN '2' THEN 'High' WHEN '3' THEN 'Urgent' WHEN '4' THEN 'Critical' ELSE 'Error' END,
+									CASE a.ticket_status WHEN '0' THEN 'Closed' WHEN '1' THEN 'Open' WHEN '2' THEN 'To Assign' ELSE 'Error' END ,
+									a.title,
+									b.name ,
+									c.department_name,
+									tu.mail,
+									tu.name 
+								FROM ".$SupportTicketsTable." a,
+								LEFT JOIN ".$SupportUserTable." b, 
+									ON	b.id=a.user_id
+								LEFT JOIN ".$SupportDepaTable." c,
+									ON c.id=a.department_id
+								LEFT JOIN ".$SupportUserTable." tu
+									ON tu.id=a.operator_id
+								WHERE a.enc_id=?
+								LIMIT 1;";
+				else//operator
+					$query="SELECT	
+									a.ref_id,
+									CASE a.priority WHEN '0' THEN 'Low' WHEN '1' THEN 'Medium' WHEN '2' THEN 'High' WHEN '3' THEN 'Urgent' WHEN '4' THEN 'Critical' ELSE 'Error' END, 
+									CASE a.ticket_status WHEN '0' THEN 'Closed' WHEN '1' THEN 'Open' WHEN '2' THEN 'To Assign' ELSE 'Error' END ,
+									a.title, 
+									b.name , 
+									b.mail_alert,
+									c.department_name, 
+									b.mail,
+									tu.name 
+								FROM ".$SupportTicketsTable." a,
+								LEFT JOIN ".$SupportUserTable." b, 
+									ON	b.id=a.user_id
+								LEFT JOIN ".$SupportDepaTable." c,
+									ON c.id=a.department_id
+								LEFT JOIN ".$SupportUserTable." tu
+									ON tu.id=a.operator_id
+								WHERE a.enc_id=?
+								LIMIT 1;";
 			}
 			else 
 				exit();
@@ -51,7 +86,24 @@ if(isset($argv[0]) && isset($argv[1]) && isset($argv[2])){
 		case 'NewTick':
 			if(is_file('../php/config/mail/newticket.txt')){
 				$file=file('../php/config/mail/newticket.txt',FILE_IGNORE_NEW_LINES);
-				$query="SELECT a.ref_id,CASE a.priority WHEN '0' THEN 'Low' WHEN '1' THEN 'Medium' WHEN '2' THEN 'High' WHEN '3' THEN 'Urgent' WHEN '4' THEN 'Critical' ELSE 'Error' END, CASE a.ticket_status WHEN '0' THEN 'Closed' WHEN '1' THEN 'Open' WHEN '2' THEN 'To Assign' ELSE 'Error' END ,a.title, b.name ,c.department_name, b.mail,tu.name FROM ".$SupportTicketsTable." a, ".$SupportUserTable." b, ".$SupportDepaTable." c, ".$SupportTicketsTable." lt, ".$SupportUserTable." tu WHERE a.enc_id=? AND b.id=a.user_id AND c.id=a.department_id  AND tu.id=lt.operator_id LIMIT 1;";
+				$query="SELECT 
+							a.ref_id,
+							CASE a.priority WHEN '0' THEN 'Low' WHEN '1' THEN 'Medium' WHEN '2' THEN 'High' WHEN '3' THEN 'Urgent' WHEN '4' THEN 'Critical' ELSE 'Error' END,
+							CASE a.ticket_status WHEN '0' THEN 'Closed' WHEN '1' THEN 'Open' WHEN '2' THEN 'To Assign' ELSE 'Error' END ,
+							a.title,
+							b.name ,
+							c.department_name, 
+							b.mail,
+							tu.name 
+							FROM ".$SupportTicketsTable." a,
+							LEFT JOIN ".$SupportUserTable." b, 
+								ON	b.id=a.user_id
+							LEFT JOIN ".$SupportDepaTable." c,
+								ON c.id=a.department_id
+							LEFT JOIN ".$SupportUserTable." tu
+								ON tu.id=a.operator_id
+							WHERE a.enc_id=?
+							LIMIT 1;";
 			}
 			else 
 				exit();
@@ -60,7 +112,23 @@ if(isset($argv[0]) && isset($argv[1]) && isset($argv[2])){
 		case 'AssTick':
 			if(is_file('../php/config/mail/assigned.txt')){
 				$file=file('../php/config/mail/assigned.txt',FILE_IGNORE_NEW_LINES);
-				$query="SELECT a.ref_id,CASE a.priority WHEN '0' THEN 'Low' WHEN '1' THEN 'Medium' WHEN '2' THEN 'High' WHEN '3' THEN 'Urgent' WHEN '4' THEN 'Critical' ELSE 'Error' END, CASE a.ticket_status WHEN '0' THEN 'Closed' WHEN '1' THEN 'Open' WHEN '2' THEN 'To Assign' ELSE 'Error' END ,a.title, b.name ,c.department_name, tu.mail,tu.name FROM ".$SupportTicketsTable." a, ".$SupportUserTable." b, ".$SupportDepaTable." c, ".$SupportTicketsTable." lt, ".$SupportUserTable." tu WHERE a.enc_id=? AND b.id=a.user_id AND c.id=a.department_id  AND tu.id=lt.operator_id LIMIT 1;";
+				$query="SELECT a.ref_id,
+								CASE a.priority WHEN '0' THEN 'Low' WHEN '1' THEN 'Medium' WHEN '2' THEN 'High' WHEN '3' THEN 'Urgent' WHEN '4' THEN 'Critical' ELSE 'Error' END,
+								CASE a.ticket_status WHEN '0' THEN 'Closed' WHEN '1' THEN 'Open' WHEN '2' THEN 'To Assign' ELSE 'Error' END ,
+								a.title, 
+								b.name ,
+								c.department_name,
+								tu.mail,
+								tu.name 
+							FROM ".$SupportTicketsTable." a,
+							LEFT JOIN ".$SupportUserTable." b, 
+								ON	b.id=a.user_id
+							LEFT JOIN ".$SupportDepaTable." c,
+								ON c.id=a.department_id
+							LEFT JOIN ".$SupportUserTable." tu
+								ON tu.id=a.operator_id
+							WHERE a.enc_id=?
+							LIMIT 1;";
 			}
 			else 
 				exit();
